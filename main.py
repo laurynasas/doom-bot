b4fbb7e834d310840afca6bc0f5c60a69a787f8f
Laurynas Tamulevicus <laurynas@opinew.com>
2017-10-29 00:47:02 +0100

    added sol path parsing

--- main.py
+++ main.py
@@ -11,6 +11,12 @@ if __name__ == "__main__":
 
     init_postion = get_position()
     print init_postion
+    solution_coords = []
+    input = open("solution_path.txt", 'r')
+    with input:
+        for line in input.readlines():
+            solution_coords.append((float(line.split(" | ")[0]), float(line.split(" | ")[1])))
+    solution_coords.reverse()
     # url = 'http://{}:{}/api/player/actions'.format(RESTFUL_HOST, RESTFUL_PORT)
     # payload = {"type": "forward", "amount": 100}
     # logging.warn('Calling {} with payload {}'.format(url, payload))
